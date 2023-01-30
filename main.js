461ae65597839d9a588dd5531b0e11f38ecf7a63
Hoang-Fe <lamduchoa96@gmail.com>
2023-01-30 17:57:28 +0700

    push

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as dat from "dat.gui";
 import { DirectionalLight } from "three";
+import backGround from "./img/1.jpg";
+import backGround1 from "./img/2.jpg";
 
 const scene = new THREE.Scene();
 
@@ -19,23 +21,30 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#bg"),
 });
-const directionalLight = new THREE.DirectionalLight(0xfffffff, 1);
-scene.add(directionalLight);
-const directionalHelper = new THREE.DirectionalLightHelper(
-  directionalLight,
-  10
-);
-const directionalShadow = new THREE.CameraHelper(
-  directionalLight.shadow.camera
-);
-scene.add(directionalHelper);
-scene.add(directionalShadow);
-directionalLight.shadow.camera.top = 50;
-directionalLight.shadow.camera.bottom = -50;
-directionalLight.shadow.camera.left = -50;
-directionalLight.shadow.camera.right = 50;
-directionalLight.castShadow = true;
+// const directionalLight = new THREE.DirectionalLight(0xfffffff, 1);
+// scene.add(directionalLight);
+// const directionalHelper = new THREE.DirectionalLightHelper(
+//   directionalLight,
+//   10
+// );
+// const directionalShadow = new THREE.CameraHelper(
+//   directionalLight.shadow.camera
+// );
+// scene.add(directionalHelper);
+// scene.add(directionalShadow);
+// directionalLight.shadow.camera.top = 50;
+// directionalLight.shadow.camera.bottom = -50;
+// directionalLight.shadow.camera.left = -50;
+// directionalLight.shadow.camera.right = 50;
+// directionalLight.castShadow = true;
+// const TextureLoader = new THREE.TextureLoader();
+// scene.background = TextureLoader.load();
 
+const spotLight = new THREE.SpotLight(0xfffffff);
+scene.add(spotLight);
+spotLight.castShadow = true;
+const SLightHelper = new THREE.SpotLightHelper(spotLight);
+scene.add(SLightHelper);
 renderer.shadowMap.enabled = true;
 renderer.setPixelRatio(window.devicePixelRatio);
 const orbit = new OrbitControls(camera, renderer.domElement);
@@ -74,6 +83,8 @@ const options = {
   torusX: 0,
   torusZ: 0,
   lightColor: 0xffffff,
+  rotateX: 0,
+  rotateY: 0,
 };
 
 // obj Gui
@@ -94,36 +105,56 @@ gui.add(options, "torusX", -60, 60, 1).onChange((e) => {
 gui.add(options, "torusZ", -60, 60, 1).onChange((e) => {
   torus.position.z = e;
 });
+gui.add(options, "rotateX", -90, 90, 0.5).onChange((e) => {
+  torus.rotation.x = e;
+});
+gui.add(options, "rotateY", -90, 90, 0.5).onChange((e) => {
+  torus.rotation.y = e;
+});
 // light gui
 const lightOptions = {
   lightColor: 0xffffff,
-  lightX: -30,
-  lightY: 50,
+  lightX: 0,
+  lightY: 100,
   lightZ: 0,
+  angle: 0.2,
+  penumbra: 0,
+  intensity: 1,
 };
 const lightGui = new dat.GUI();
 
-lightGui.addColor(lightOptions, "lightColor").onChange((e) => {
-  directionalLight.color.set(e);
-});
-lightGui.add(lightOptions, "lightY", 0, 60, 1).onChange((e) => {
-  directionalLight.position.y = e;
-});
-lightGui.add(lightOptions, "lightX", -60, 60, 1).onChange((e) => {
-  directionalLight.position.x = e;
-  directionalHelper.scale = e;
-});
-lightGui.add(lightOptions, "lightZ", -60, 60, 1).onChange((e) => {
-  directionalLight.position.z = e;
-});
+lightGui.addColor(lightOptions, "lightColor");
+lightGui.add(lightOptions, "lightY", 0, 100, 1);
 
-let step = 0;
-let speed = 0.01;
+lightGui.add(lightOptions, "lightX", -100, 100, 1);
+
+lightGui.add(lightOptions, "lightZ", -100, 100, 1);
+
+lightGui.add(lightOptions, "angle", 0, 1, 0.1);
+lightGui.add(lightOptions, "penumbra", 0, 1, 0.1);
+lightGui.add(lightOptions, "intensity", 0, 1, 0.1);
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+
+scene.background = cubeTextureLoader.load([
+  backGround1,
+  backGround,
+  backGround1,
+  backGround,
+  backGround1,
+  backGround1,
+]);
 function animate() {
   requestAnimationFrame(animate);
-  step += speed;
-  // torus.position.y = 10 * Math.abs(Math.sin(step));
 
+  // torus.position.y = 10 * Math.abs(Math.sin(step));
+  spotLight.color.set(lightOptions.lightColor);
+  spotLight.position.y = lightOptions.lightY;
+  spotLight.position.x = lightOptions.lightX;
+  spotLight.position.z = lightOptions.lightZ;
+  spotLight.angle = lightOptions.angle;
+  spotLight.intensity = lightOptions.intensity;
+  spotLight.penumbra = lightOptions.penumbra;
+  SLightHelper.update();
   renderer.render(scene, camera);
 }
 animate();
